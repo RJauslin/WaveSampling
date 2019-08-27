f240935da7f8972ccb89ea11c6a3f1d5bc296e29
JAUSLIN Raphaël <raphael.jauslin@unine.ch>
2019-08-27 17:06:36 +0200

    add use_data

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' @encoding UTF-8 
+#' Data from Experiment 1
+#'
+#' This is data from the first experiment ever to try XYZ using Mechanical
+#' Turk workers.
+#'
+"SwissCanton"
+
+#' @encoding UTF-8 
+#' Data from Experiment 1
+#'
+#' This is data from the first experiment ever to try XYZ using Mechanical
+#' Turk workers.
+#'
+"Swiss"
---
+++ data-raw/SwissCanton.R
@@ -0,0 +1,8 @@
+
+SwissCanton <- read_sf("C:/Users/jauslinr/switchdrive/SwissDATA/ggg_2019-LV95/shp/g1k19.shp")
+usethis::use_data(SwissCanton)
+
+
+Swiss <- read_sf("C:/Users/jauslinr/switchdrive/SwissDATA/ggg_2019-LV95/shp/g1l19.shp")
+usethis::use_data(Swiss)
+
